9340be5c29dd0d2b1bff9b68910a6acc3d843c87
Alex <Alex@DESKTOP-MGVA200>
2023-02-28 23:33:03 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCProject.Models;
+
+namespace FirstMVCProject.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVM = new DogViewModel();
+            return View(dogVM);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel) 
+        {
+
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello() 
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstMVCProject.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
